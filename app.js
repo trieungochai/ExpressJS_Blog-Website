f9c93e16d4122d42ada0e93457c5dabfdc3c0d9a
Cai <trieungochai.dev@gmail.com>
2021-09-16 11:20:45 +0700

    Challenge 5

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.get("/", function (req, res) {
   res.render("home", { startingContent: homeStartingContent });
 });
 
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
 app.listen(3000, function () {
   console.log("The server is running on port 3000!");
 });
