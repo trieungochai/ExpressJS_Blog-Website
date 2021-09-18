e71f2bf4a0cd703c50e0b93f416b3380d240b22f
Cai <trieungochai.dev@gmail.com>
2021-09-18 16:17:54 +0700

    Challenge 19

--- app.js
+++ app.js
@@ -52,7 +52,10 @@ app.get("/posts/:postName", function (req, res) {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
-      console.log("Match found!");
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
     } else {
       console.log("Not a match!");
     }
